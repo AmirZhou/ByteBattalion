10aabe0c8d22414db488ffc6b5f1c4433779be30
Amir Yue Zhou <amirzhou001@gmail.com>
2024-07-16 05:51:23 -0600

    rm nested git repo

--- typeORM-playground-v1/src/data-source.ts
+++ typeORM-playground-v1/src/data-source.ts
@@ -1,6 +1,16 @@
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 import { User } from './entity/User';
+import { UserSetting } from './entity/UserSetting';
+import { Progress } from './entity/Progress';
+import { Priority } from './entity/Priority';
+import { Notification } from './entity/Notification';
+import { Milestone } from './entity/Milestone';
+import { AIRecommendation } from './entity/AIRecommendation';
+import { Task } from './entity/Task';
+import { Tag } from './entity/Tag';
+import { Goal } from './entity/Goal';
+import { Category } from './entity/Category';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
@@ -11,7 +21,19 @@ export const AppDataSource = new DataSource({
   database: 'typeORM-playground-v1',
   synchronize: true,
   logging: false,
-  entities: [User],
+  entities: [
+    User,
+    UserSetting,
+    Task,
+    Tag,
+    Progress,
+    Priority,
+    Notification,
+    Milestone,
+    Goal,
+    Category,
+    AIRecommendation,
+  ],
   migrations: [],
   subscribers: [],
 });
--- typeORM-playground-v1/src/entity/Tag.ts
+++ typeORM-playground-v1/src/entity/Tag.ts
@@ -1,14 +1,20 @@
-import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from "typeorm";
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity('tags')
 export class Tag {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({type: 'varchar', length: 255})
+  @Column({ type: 'varchar', length: 255 })
   name: string;
 
-  @Column({type: 'text', nullable: true})
+  @Column({ type: 'text', nullable: true })
   description: string;
 
   @CreateDateColumn()
