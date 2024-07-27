c0ea032f2c4249f0ee3821f074c9caf8b4ade6c4
AmirZhou <amirzhou001@gmail.com>
2024-07-27 08:41:58 -0600

    add handlers

---
+++ messages/src/messages/messages.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get, Post } from '@nestjs/common';
+
+@Controller('messages')
+export class MessagesController {
+  @Get()
+  listMessages() {
+    return 'List of messages';
+  }
+
+  @Get(':id')
+  getMessage() {
+    return 'A single message';
+  }
+
+  @Post()
+  createMessage() {
+    return 'Create a message';
+  }
+}
