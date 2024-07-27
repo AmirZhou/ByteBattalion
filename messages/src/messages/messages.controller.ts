f3be95d9c190235f8ef1c38931aa4393fd72d7a5
Amir Yue Zhou <amirzhou001@gmail.com>
2024-07-27 11:13:47 -0600

    create Dto class, update controller and main.ts
    apply the dto class to the createMessage method
    in the controller. That's how we can use the dto
     class

--- messages/src/messages/messages.controller.ts
+++ messages/src/messages/messages.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
 
 @Controller('messages')
 export class MessagesController {
@@ -14,7 +15,7 @@ export class MessagesController {
   }
 
   @Post()
-  createMessage(@Body() body: any) {
+  createMessage(@Body() body: CreateMessageDto) {
     console.log(body);
     return 'Create a message';
   }
