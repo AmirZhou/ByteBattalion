b53531d0637e720b867a05a484d8168f03ebb88f
AmirZhou <amirzhou001@gmail.com>
2024-07-28 06:48:46 -0600

    DI practice

--- di/src/computer/computer.service.ts
+++ di/src/computer/computer.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { DiskService } from 'src/disk/disk.service';
+import { CpuService } from 'src/cpu/cpu.service';
 
 @Injectable()
-export class ComputerService {}
+export class ComputerService {
+  constructor(
+    private diskService: DiskService,
+    private cpuService: CpuService,
+  ) {}
+
+  run() {
+    return [this.cpuService.compute(5, 6), this.diskService.getData()];
+  }
+}
