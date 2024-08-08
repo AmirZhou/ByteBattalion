8849feef9e0422f2f11eb4588a81ea4b38693250
Amir Yue Zhou <amirzhou001@gmail.com>
2024-08-08 15:00:35 -0600

    use Query() and ParseIntPipe in controller

--- mycv/src/users/users.controller.ts
+++ mycv/src/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Patch,
+  Param,
+  Body,
+  Query,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 
@@ -9,5 +18,15 @@ export class UsersController {
   @Post('/signup')
   createUser(@Body() createUserDto: CreateUserDto) {
     this.usersService.create(createUserDto.email, createUserDto.password);
+  }
+
+  @Get()
+  async findAllUsers(@Query('email') email: string) {
+    return await this.usersService.findBy(email);
+  }
+
+  @Get('/:id')
+  async findUser(@Param('id', ParseIntPipe) id: number) {
+    return await this.usersService.findOneBy(id);
   }
 }
