8c3b8a7eeb58592ee3cf91cf20a21d679549cdca
Amir Yue Zhou <amirzhou001@gmail.com>
2024-08-09 20:37:17 -0600

    a good intercepter, but only for one handler

--- mycv/src/interceptors/serialize.interceptor.ts
+++ mycv/src/interceptors/serialize.interceptor.ts
@@ -8,16 +8,17 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { plainToInstance } from 'class-transformer';
 import { LogClassName } from 'src/custom-decorators';
-import { UserDto } from 'src/users/dtos';
 
 @LogClassName()
 @Injectable()
 export class SerializeInterceptor implements NestInterceptor {
+  constructor(private readonly dto: any) {}
+
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
       map((data: any) => {
         //Run something before the response is sent out
-        return plainToInstance(UserDto, data, {
+        return plainToInstance(this.dto, data, {
           excludeExtraneousValues: true,
         });
       }),
