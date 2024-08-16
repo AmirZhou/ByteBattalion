fd4507e85068c7c10b76321a39737cb20b60aadf
Amir Yue Zhou <amirzhou001@gmail.com>
2024-08-16 17:46:15 -0600

    use bruno as http client

--- mycv/src/users/users.module.ts
+++ mycv/src/users/users.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
-import { UsersService } from './users.service';
+import { UsersService } from './users.service' ;
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User])], // This forFeature Thing returns a module
+  providers: [UsersService],
   controllers: [UsersController],
-  providers: [UsersService]
+  exports: [],
 })
 export class UsersModule {}
