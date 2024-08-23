1d5f75494254de0f53d26a8fa9ec768051be9a33
Amir Yue Zhou <amirzhou001@gmail.com>
2024-08-23 10:13:31 -0600

    hash and salt the pwd

--- mycv/src/users/auth.service.ts
+++ mycv/src/users/auth.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { EmailAlreadyExistsException } from 'src/exceptions';
+import { randomBytes, scrypt as _scrypt, BinaryLike } from 'crypto';
+import { promisify } from 'util';
+
+const scrypt = promisify<BinaryLike, BinaryLike, number, Buffer>(_scrypt);
 
 @Injectable()
 export class AuthService {
@@ -14,7 +18,23 @@ export class AuthService {
     if (user.length !== 0) {
       throw new EmailAlreadyExistsException();
     }
-    // Hash the users password
+    // Generate a salt
+    const salt = randomBytes(8).toString('hex'); // This returns a Buffer, that's an object that deals with binary, a buffer for holding and manipulating binary data in memory.
+
+    // Hash the password and the salt TOGETHER
+
+    // This is the callback version, I'll have to handle the err directly in the callback
+    // let hasedPasswordWithoutPromise = '';
+    // _scrypt(password, salt, 32, (err, value) => {
+    //   if (err) {
+    //     throw err;
+    //   }
+    //   hasedPasswordWithoutPromise = value.toString('hex');
+    // });
+    const hash = await scrypt(password, salt, 32);
+    // Join the hased result and the salt TOGETHER
+
+    const result = salt + '.' + hash.toString('hex')
 
     // Create a new user
 
