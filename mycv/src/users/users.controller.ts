99440ae22352ed3476f01d19849847bdc70dc7ad
Amir Yue Zhou <amirzhou001@gmail.com>
2024-08-28 08:30:31 -0600

    implement a interceptor

--- mycv/src/users/users.controller.ts
+++ mycv/src/users/users.controller.ts
@@ -19,11 +19,10 @@ import { AuthService } from './auth.service';
 import { User } from './user.entity';
 import { UserExceptionsFilter } from 'src/exceptions';
 import { CurrentUser } from 'src/custom-decorators';
-import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
 
 @Controller('auth')
 @UseFilters(UserExceptionsFilter)
-@UseInterceptors(new SerializeInterceptor<UserDto>(UserDto), CurrentUserInterceptor)
+@UseInterceptors(new SerializeInterceptor<UserDto>(UserDto))
 // @Serialize(UserDto)   the above long code could be simplified to this,
 //if I do a easy custom decorator in the intercepter file
 export class UsersController {
--- mycv/src/users/users.module.ts
+++ mycv/src/users/users.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -9,7 +10,11 @@ import { CurrentUserInterceptor } from './interceptors/current-user.interceptor'
 
 @Module({
   imports: [TypeOrmModule.forFeature([User]), NotificationsModule], // This forFeature Thing returns a module
-  providers: [UsersService, AuthService, CurrentUserInterceptor],
+  providers: [
+    UsersService,
+    AuthService,
+    { provide: APP_INTERCEPTOR, useClass: CurrentUserInterceptor }, 
+  ],
   controllers: [UsersController],
   exports: [],
 })
