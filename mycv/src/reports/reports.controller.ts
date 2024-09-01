137da826e4f9373723af0352cc8369cf9ddc308e
Amir Yue Zhou <amirzhou001@gmail.com>
2024-09-01 12:15:55 -0600

    serialize the response using Transform, and eXPOSE

--- mycv/src/reports/reports.controller.ts
+++ mycv/src/reports/reports.controller.ts
@@ -1,7 +1,11 @@
-import { Controller, Post, Body, UseGuards, Get } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, Get, UseInterceptors } from '@nestjs/common';
 import { ReportsService } from './reports.service';
 import { CreateReportDto } from './dtos';
 import { SessionGuard } from '../guards/session.guard';
+import { CurrentUser } from '../custom-decorators';
+import { User } from '../users/user.entity';
+import { SerializeInterceptor } from '../interceptors';
+import { ReportDto} from './dtos'
 
 @Controller('reports')
 export class ReportsController {
@@ -9,8 +13,9 @@ export class ReportsController {
 
   @Post()
   @UseGuards(SessionGuard)
-  create(@Body() createReportDto: CreateReportDto) {
-    return this.reportsService.create(createReportDto);
+  @UseInterceptors(new SerializeInterceptor<ReportDto>(ReportDto))
+  create(@Body() createReportDto: CreateReportDto, @CurrentUser() user: User) {
+    return this.reportsService.create(createReportDto, user);
   }
 
   @Get()
--- mycv/src/reports/reports.service.ts
+++ mycv/src/reports/reports.service.ts
@@ -3,16 +3,14 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateReportDto } from './dtos';
 import { Report } from './report.entity';
+import { User } from '../../src/users/user.entity';
 
 @Injectable()
 export class ReportsService {
-  constructor(
-    @InjectRepository(Report) private repo: Repository<Report>,
-  ) {}
+  constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
 
-  create(createReportDto: CreateReportDto) {
-    const report = this.repo.create(createReportDto)
+  create(createReportDto: CreateReportDto, user: User) {
+    const report = this.repo.create({ ...createReportDto, user });
     return this.repo.save(report);
   }
-
 }
